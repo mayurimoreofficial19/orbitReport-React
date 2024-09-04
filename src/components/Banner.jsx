d4182a3cec295cfe119d392baf1789e966a8da5a
mayurimoreofficial19 <mayurimoreofficial19@gmail.com>
2024-09-04 06:59:28 -0500

    GA-6 Submitted

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,10 +2,10 @@ import "./styling.css";
 
 function Banner() {
   return (
-    <>
+    <header>
       <h1>Orbit Report</h1>
       <p>Click on the buttons to see the satellites in that orbit type</p>
-    </>
+    </header>
   );
 }
 
